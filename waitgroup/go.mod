c77544e93ebdd6190feed7e7325993ef88d51fbf
蓝 <lan@landeMacBook-Pro.local>
2019-06-03 15:23:30 +0800

    feat: 001 waitgroup

---
+++ waitgroup/go.mod
@@ -0,0 +1,5 @@
+module lanhao/wg-1
+
+go 1.12
+
+require github.com/davecgh/go-spew v1.1.1
---
+++ waitgroup/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"sync"
+)
+
+/**
+ *
+ */
+type WeatherBody struct {
+	WeatherInfo `json:"weatherinfo"`
+}
+
+type WeatherInfo struct {
+	// 城市
+	City string `json:"city"`
+	// 最低温度
+	Low string `json:"temp1"`
+	// 最高温度
+	High string `json:"temp2"`
+}
+
+func main() {
+	result := []WeatherInfo{}
+	// 声明一个 waitGroup 并增加两个等待信号
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	// 使用协程请求北京天气情况
+	go func() {
+		resp, err := http.Get("http://www.weather.com.cn/data/cityinfo/101010100.html")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+		weatherBody := WeatherBody{}
+		json.Unmarshal(body, &weatherBody)
+		weatherInfo := weatherBody.WeatherInfo
+		result = append(result, weatherInfo)
+		// 告诉 wg 我已经完成任务
+		wg.Done()
+	}()
+
+	// 使用协程请求成都天气情况
+	go func() {
+		resp, err := http.Get("http://www.weather.com.cn/data/cityinfo/101270101.html")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+		weatherBody := WeatherBody{}
+		json.Unmarshal(body, &weatherBody)
+		weatherInfo := weatherBody.WeatherInfo
+		result = append(result, weatherInfo)
+		// 告诉 wg 我已经完成任务
+		wg.Done()
+	}()
+
+	wg.Wait()
+	fmt.Printf("%+v", result)
+}
